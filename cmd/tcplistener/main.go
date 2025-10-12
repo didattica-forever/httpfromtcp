9043406d1cbb0ff0eb0ed7eb6dcecb3e0add7393
Paolo M. <didattica@gmx.com>
2025-10-12 05:27:35 +0200

    parsing hpp request

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,56 +1,67 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net"
-	"os"
+	"strings"
 )
 
-func check(e error) {
-	if e != nil {
-		//panic(e)
-		log.Fatalf("could not open %s\n", e)
-	}
-}
-
-const inputFilePath = "messages.txt"
-
-func main_from_file() {
-	f, err := os.Open(inputFilePath)
-	if err != nil {
-		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
-	}
-
-	fmt.Printf("Reading data from %s\n", inputFilePath)
-	fmt.Println("=====================================")
-
-	linesChan := getLinesChannel(f)
-
-	for line := range linesChan {
-		fmt.Println("read:", line)
-	}
-}
+const port = ":42069"
 
 func main() {
-	listener, err := net.Listen("tcp", ":42069")
+	listener, err := net.Listen("tcp", port)
 	if err != nil {
-		log.Fatalf("could not obtain listener: %s\n", err)
+		log.Fatalf("error listening for TCP traffic: %s\n", err.Error())
 	}
 	defer listener.Close()
 
+	fmt.Println("Listening for TCP traffic on", port)
 	for {
-		connection, err := listener.Accept()
+		conn, err := listener.Accept()
 		if err != nil {
-			log.Fatalf("could not accept connection: %s\n", err)
+			log.Fatalf("error: %s\n", err.Error())
 		}
-		fmt.Printf("Connection accepted \n")
+		fmt.Println("Accepted connection from", conn.RemoteAddr())
 
-		linesChan := getLinesChannel(connection)
+		linesChan := getLinesChannel(conn)
 
 		for line := range linesChan {
-			fmt.Println("read:", line)
+			fmt.Println(line)
 		}
+		fmt.Println("Connection to ", conn.RemoteAddr(), "closed")
 	}
+}
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	go func() {
+		defer f.Close()
+		defer close(lines)
+		currentLineContents := ""
+		for {
+			b := make([]byte, 8, 8)
+			n, err := f.Read(b)
+			if err != nil {
+				if currentLineContents != "" {
+					lines <- currentLineContents
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				return
+			}
+			str := string(b[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				lines <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
+				currentLineContents = ""
+			}
+			currentLineContents += parts[len(parts)-1]
+		}
+	}()
+	return lines
 }
